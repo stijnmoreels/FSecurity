f84830fdd7aaf474b045c6679a688a96f3f5650f
Stijn Moreels <stijn.moreels@codit.eu>
2018-09-19 13:16:58 +0200

    Update realpath

--- build.sh
+++ build.sh
@@ -71,6 +71,20 @@ fi
 
 run $PAKET_EXE restore
 
+# liberated from https://stackoverflow.com/a/18443300/433393
+realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
 TOOL_PATH=$(realpath .fake)
 FAKE="$TOOL_PATH"/fake
 

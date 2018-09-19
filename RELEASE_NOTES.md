4f6527bd570d458c2b75ead3dfd61a3a174a1b26
Stijn Moreels <stijn.moreels@codit.eu>
2018-09-19 12:41:12 +0200

    Remove source

--- RELEASE_NOTES.md
+++
@@ -1,3 +0,0 @@
-#### 1.0.7 - Februari 2018
-
-Initial release testing
--- tests/FSec.Tests/VulnResult.fs
+++
@@ -1,9 +0,0 @@
-﻿[<AutoOpen>]
-module VulnResult
-
-/// Represents a type that acts as response of a SUT to indicate wheter or not the given payload results in a vulnerable system.
-type VulnResult = 
-    | Prevented 
-    | Vulnerable
-
-let testVuln f = try f () with | _ -> Vulnerable
